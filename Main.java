eadb4820e9716d1135f79cc6c15e9cc61dfedd86
Damla Su Bilge <109955877+nosey-dewdrop@users.noreply.github.com>
2025-05-16 19:01:14 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -4,7 +4,7 @@ import java.awt.event.ActionListener;
 import java.util.*;
 import javax.swing.*;
 
-public class FramePanelLabel{
+public class Main{
 
       public static void main(String[] args){
 
